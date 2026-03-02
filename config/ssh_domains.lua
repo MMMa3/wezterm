57b8b5d92a13cc6692eecf9b04812955c12c4364
MMMa3 <anmu19473@gmail.com>
2026-03-02 16:23:03 +0800

    ssh key path
    Edited ssh key path

--- config/ssh_domains.lua
+++ config/ssh_domains.lua
@@ -1,15 +1,15 @@
 local M = {}
 
 function M.apply(config)
-    config.ssh_domains = {
-        {
-            name = "Hostdare0.mmm.com",
-            remote_address = "212.103.62.177:22",
-            username = "root",
-            -- 如果需要可以指定私钥路径
-            -- ssh_option = { identityfile = "~/.ssh/id_rsa" },
-        },
-    }
+	config.ssh_domains = {
+		{
+			name = "Hostdare0.mmm.com",
+			remote_address = "212.103.62.177:22",
+			username = "root",
+			-- 如果需要可以指定私钥路径
+			ssh_option = { identityfile = "~/.ssh/id_ed25519" },
+		},
+	}
 end
 
 return M
